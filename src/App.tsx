a0ee3d2249d5137293fc0caa5a52b3e32c41f922
Patrick Silva <contato@patricksilva.dev>
2021-06-22 21:13:09 -0300

    feat: :sparkles: adding Home and NewRoom page with styles and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+ 
+import { Home } from './pages/Home';
+import { NewRoom } from "./pages/NewRoom";
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonHTMLAttributes } from 'react';
+
+import "../styles/button.scss";
+
+export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button className="button" {...props} />
+  )
+}
